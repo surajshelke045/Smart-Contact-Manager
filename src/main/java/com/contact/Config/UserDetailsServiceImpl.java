3878a95a1b015ca7ec283f5e1ea045e28731bde8
suraj shelke <shelkesuraj2000@gmail.com>
2023-10-10 13:26:23 +0530

    create Login System with Spring Security

---
+++ src/main/java/com/contact/Config/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.contact.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.contact.Repository.UserRepository;
+import com.contact.entities.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	
+		// fetching user from database
+		
+		User user=userRepository.getUserByUserName(username);
+		
+		if(user==null) {
+			
+			throw new UsernameNotFoundException("User could not found");
+		}
+	
+		CustomUserDetails customUserDetails=new CustomUserDetails(user);
+		
+		return customUserDetails;
+	}
+
+}
--- src/main/java/com/contact/Controllers/HomeController.java
+++ src/main/java/com/contact/Controllers/HomeController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,6 +24,9 @@ public class HomeController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@RequestMapping("/")
 	public String home(Model model) {
@@ -62,6 +66,7 @@ public class HomeController {
 			user.setRole("ROLE_USER");
 			user.setEnabled(true);
 			user.setImageUrl("default.png");
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
 
 			System.out.println("Agreement : " + agreement);
 			System.out.println("User : " + user);
