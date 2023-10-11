6403706b38521cad63bc34efa3e5e60af3a8d948
suraj shelke <shelkesuraj2000@gmail.com>
2023-10-11 23:27:05 +0530

    Create upload image endpoint

--- src/main/java/com/contact/Controllers/UserController.java
+++ src/main/java/com/contact/Controllers/UserController.java
@@ -1,16 +1,24 @@
 package com.contact.Controllers;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.contact.Helper.Message;
 import com.contact.Repository.UserRepository;
@@ -20,52 +28,71 @@ import com.contact.entities.User;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@ModelAttribute
-	public void addCommonData(Model model,Principal principal) {
-		String username =principal.getName();
-		System.out.println("USERNAME :"+username);
-		
-		User user=userRepository.getUserByUserName(username);
-		
-		System.out.println("User : "+user);
-		
-		model.addAttribute("user",user);
+	public void addCommonData(Model model, Principal principal) {
+		String username = principal.getName();
+		System.out.println("USERNAME :" + username);
+
+		User user = userRepository.getUserByUserName(username);
+
+		System.out.println("User : " + user);
+
+		model.addAttribute("user", user);
 	}
-	
+
 	@RequestMapping("/index")
-	public String dashboard(Model model,Principal principal) {
-		
-		model.addAttribute("title","User Dashboard");
-		
+	public String dashboard(Model model, Principal principal) {
+
+		model.addAttribute("title", "User Dashboard");
+
 		return "normal/user_dashboard";
 	}
-	
+
 	@GetMapping("/add-contact")
 	public String openAddContactForm(Model model) {
-		
-		model.addAttribute("title","Add Contact");
-		model.addAttribute("contact",new Contact());
-		return "normal/add_contact_form";
-		
-	}
-	
-	@PostMapping("/process-contact")
-	public String processContact(@ModelAttribute Contact contact,Principal principal,HttpSession session) {
-		String name=principal.getName();
-		User user=this.userRepository.getUserByUserName(name);
-		contact.setUser(user);
 
-		user.getContacts().add(contact);
-		session.setAttribute("message", new Message("Successfully Registered !!","alert-success" ));
-		
-		this.userRepository.save(user);
-		
-		System.out.println("DATA :"+contact);
-		System.out.println("Added to DATABASE" );
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		return "normal/add_contact_form";
+
+	}
+
+	@PostMapping("/process-contact")
+	public String processContact(@ModelAttribute Contact contact, @RequestParam("ProfileImage") MultipartFile file,
+			Principal principal, HttpSession session) {
+		try {
+			String name = principal.getName();
+			User user = this.userRepository.getUserByUserName(name);
+
+			if (file.isEmpty()) {
+					System.out.println("Image is Empty");
+				
+			} else {
+				// upload file and update name of file
+				contact.setImage(file.getOriginalFilename());
+				File saveFile = new ClassPathResource("static/img").getFile();
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("Image is Uploaded..!");
+			}
+			contact.setUser(user);
+
+			user.getContacts().add(contact);
+			session.setAttribute("message", new Message("Contact Added Successfully !!", "alert-success"));
+
+			this.userRepository.save(user);
+
+			System.out.println("DATA :" + contact);
+			System.out.println("Added to DATABASE");
+		} catch (Exception e) {
+			session.setAttribute("message", new Message("Something went wrong..! "+e.getMessage(),"alert-danger" ));
+			System.out.println("ERROR : " + e.getMessage());
+			e.printStackTrace();
+		}
 		return "normal/add_contact_form";
 	}
 
